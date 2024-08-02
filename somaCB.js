a55be326360b4a0263eb6750f26b86108ad9a1a2
Caua <caua.goldenboyy@gmail.com>
2024-08-02 18:00:33 -0300

    subiu

--- somaCB.js
+++ somaCB.js
@@ -2,14 +2,18 @@ import express from 'express';
 
 const servidor = express()
 
-servidor.use(express);
+
 
 
 servidor.get('/calc/soma/:n1:n2', (req, resp)  => {
     let n1 = Number(req.params.n1)
-    let n2 = NUmber(req.params.n2)
+    let n2 = Number(req.params.n2)
 
-    resp.send('a soma é ' + n2+n1)
+    let soma  = n1+n2
+
+    resp.send({
+        soma:soma
+    })
 })
 
 
